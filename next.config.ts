f8a3e70a5dd2ce583725027b2a52a022c736a3e6
Shamun Khatri <shamunkhatri@gmail.com>
2025-08-20 16:13:54 +0530

    feat: configure image loading from S3 bucket in Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    // Allow loading images from the S3 bucket
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "portfolio-shamun.s3.ap-south-1.amazonaws.com",
+        pathname: "/images/**",
+      },
+      // If images might be at root without /images/ prefix, uncomment below:
+      // { protocol: "https", hostname: "portfolio-shamun.s3.ap-south-1.amazonaws.com", pathname: "**" },
+    ],
+  },
 };
 
 export default nextConfig;
