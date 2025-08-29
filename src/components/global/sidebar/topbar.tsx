d71e03fe372e96f50a4155b798750effebc0c877
Shamun Khatri <shamunkhatri@gmail.com>
2025-08-29 16:17:01 +0530

    feat: add TopBar component for improved layout and navigation

---
+++ src/components/global/sidebar/topbar.tsx
@@ -0,0 +1,19 @@
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import TopBarBreadcrumb from "./top-bar-breadcrumb";
+import { ModeToggle } from "../toggle-theme";
+
+export const TopBar = () => {
+  return (
+    <header className="flex sticky top-0 bg-background h-16 shrink-0 items-center gap-2 border-b px-4 z-10">
+      <SidebarTrigger className="-ml-1" />
+      <Separator orientation="vertical" className="mr-2 h-4" />
+      <TopBarBreadcrumb />
+      <div className="ml-auto">
+        <ModeToggle />
+      </div>
+    </header>
+  );
+};
+
+export default TopBar;
