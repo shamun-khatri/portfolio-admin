13d78c525132cd0e2e49f39d5d3bc448f671e7d1
Shamun Khatri <shamunkhatri@gmail.com>
2025-08-29 16:16:44 +0530

    feat: replace header with TopBar component in layout for improved structure

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import { AppSidebar } from "@/components/global/sidebar";
+import TopBar from "@/components/global/sidebar/topbar";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -21,28 +22,7 @@ export default function Page({ children }: { children: ReactNode }) {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator
-              orientation="vertical"
-              className="mr-2 data-[orientation=vertical]:h-4"
-            />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    Building Your Application
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
+        <TopBar />
         <div className="p-4 pt-0">{children}</div>
       </SidebarInset>
     </SidebarProvider>
