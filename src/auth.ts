f01d19247a89be5a15ebd78ca9b46439df8d92a6
shamun khatri <shamunkhatri@gmail.com>
2026-01-23 22:17:44 +0530

    fix: ensure domain is set for cookies in production environment

--- src/auth.ts
+++ src/auth.ts
@@ -14,6 +14,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         sameSite: "none",
         path: "/",
         secure: true,
+        domain: process.env.NODE_ENV === "production" ? ".shamunkhatri.me" : undefined,
       },
     },
     callbackUrl: {
@@ -23,6 +24,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         sameSite: "lax",
         path: "/",
         secure: false,
+        domain: process.env.NODE_ENV === "production" ? ".shamunkhatri.me" : undefined,
       },
     },
     csrfToken: {
@@ -32,6 +34,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         sameSite: "lax",
         path: "/",
         secure: false,
+        domain: process.env.NODE_ENV === "production" ? ".shamunkhatri.me" : undefined,
       },
     },
   },
